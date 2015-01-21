44c2c146a564b7aa401d6cdfd9bc7c4dbdf05cdd
sofiaqiriazi <splushys@gmail.com>
2015-01-21 16:50:59 +0100

    The main stucture of the web app is finished. The tabs are fully constructed to show the runs for each project

--- periodics.py
+++ periodics.py
@@ -2,6 +2,7 @@ import os
 import logging
 import json
 import time
+import datetime
 from datetime import date, timedelta
 from flask import Flask, request, session, g, redirect, url_for, abort, \
 	render_template, flash
@@ -50,7 +51,8 @@ def main_projects():
 
 @app.route('/nightlies-periodic/latestNews/',methods=['GET'])
 def latest_news(project=None):
-
+    
+    #get all the configuration files
     configs = g.db.view('periodic/periodicConfigs',reduce=False)
     temp = list()
     for c in configs:
@@ -64,17 +66,64 @@ def applicationNews(application):
 
     data = []
     configs = g.db.view('periodic/periodicConfigs',reduce=False,key=application)
+    #for each configuration file inside the database ask for test results
     for c in configs:
-        name_criteria = c['value']['filename']
-        time_criteria = c['value']['schedule_time']
+        #name_criteria = c['value']['filename']
+        #time_criteria = c['value']['schedule_time']
+        #results = g.db.view('periodic/periodicTests', startkey=[name_criteria,time_criteria])
+        #gather information for the specific config file
+        #for r in results:
+            #data.append(r)
+        data.append(c)
+
+    #it never is this one cause the data is made up that is why we add it hardcoded
+    #commenting this once real related data appear
+    if not data:
         name_criteria = "lhcb-gaudi-head.536.Brunel.x86_64-slc6-gcc48-test.perf.[UsePRConfig].lhcbpr.PRTEST-COLLISION12-1000"
         time_criteria = "2014-11-28T00:00:00"
         results = g.db.view('periodic/periodicTests', startkey=[name_criteria,time_criteria])
-        for r in results:
-            data.append(r)
+        #gather information for the specific config file
+    #    for r in results:
+    #        data.append(r)
+    #        data.append(r)
+
+    data = json.dumps(data)
+    
+    return data
+
+@app.route('/nightlies-periodic/<filename_time>', methods=['GET'])
+def slotTestCounts(filename_time):
+    
+    data = []
+    variables = filename_time.rsplit('|',1)
+    filename = variables[0]
+    time = (variables[1]).split("T")[0]
+    seconds = (variables[1]).split("T")[1]
+
+    
+    #find only the runs that have count equal to one
+    #on the same date as the config
+    tests_count_one = g.db.view('periodic/testFirstCount', key=[filename ,time ,"1" ])
+    
+    ftr = [3600, 60, 1]
+    scheduled_on =sum([a*b for a,b in zip(ftr, map(int,seconds.split(':')))])
+    trigger = variables[1]
+    #find the run with smallest differnce between the scheduled time and the triggered time
+    dif = 86400
+    for t in tests_count_one:
+        doc_trigger_on = t["value"]["trigger_on"].split('T')[1]
+        triggered_on = sum([a*b for a,b in zip(ftr, map(int,doc_trigger_on.split(':')))])
+        if triggered_on-scheduled_on < dif:
+            trigger = t["value"]["trigger_on"]
+
+
+    test_runs = g.db.view('periodic/testCounts', key=[filename, trigger])
+    for r in test_runs:
+        data.append(r)
 
     data = json.dumps(data)
     return data
+
 
 if __name__=='__main__':
 	app.run()
--- static/periodics/js/periodic.js
+++ static/periodics/js/periodic.js
@@ -1,128 +1,211 @@
 function loadMainApplications(){
+    //ask for the main applications 
     $.get('/nightlies-periodic/mainProjects/', function(data){
-            var data = jQuery.parseJSON(data);
+            data = jQuery.parseJSON(data);
 
             ulist = $('.etabs');
-
+            //create one tab for each main application
             for (var i in data){
-            ulist.append('<li class="tab"><a href="#tabs1-'+data[i]+'">'+data[i]+'</a></li>');
-            loadApplicationContainer(data[i]);
+                ulist.append('<li class="tab"><a href="#tabs1-'+data[i]+'">'+data[i]+'</a></li>');
+                //fill the specified tab's container with a slideshow of configs
+                loadApplicationContainer(data[i]);
             }
-
-
-            }).done(function(){
+        }).done(function(){
                 $('#tab-container').easytabs();
-                });
+        });
 }
 
 function loadApplicationContainer(application){
+    //initialize container
     var container = $('#tab-container');
     container.append('<div id="tabs1-' + application +'"></div>');
 
     var tab = $('#tabs1-'+ application);
-    console.log(tab);
-    tab.append('<div style="border:solid;" class="slidee" id="slidee-'+ application +'">'+
+    tab.append('<div class="slidee" id="slidee-'+ application +'">'+
             '<ul class="app-items" id="app-items-'+application +'"></ul>'+
             '</div>');
-
+    
+    //ask for the specified application's latest events
     $.get('/nightlies-periodic/applicationNews/'+application,function(data){
             data = jQuery.parseJSON(data);
+            //for each result returned
             for (var i in data){
-                loadDetails(data[i],i,application);
+                //create a detailed description of the entry
+                loadDetails(data[i],application);
             }
 
             tabid = 'slidee-' + application;
-            }).done(function(){
+        }).done(function(){
                 loadSubSlidee(tabid);
-                });
+                loadPageSlidee('.nonitembased');
+        });
 }
 
-function loadDetails(data,i,application){
+function loadTestResults(utable,filename,schedule_time){ 
+    
+    content = filename + '|' + schedule_time;
+    $.get('/nightlies-periodic/'+content, function(data){
+            data = jQuery.parseJSON(data);
+            //CREATE A SLIDEE OF TRIANGLE
+            if (data.length > 0){
+                 
+                var slidee = $('.countwrap').clone();
+                console.log(slidee);
+                slidee.addClass("timewrap");
+                $(slidee).removeClass("countwrap");
+        
+                var ulist = document.createElement('ul');
+                var listitem = document.createElement('li');
+                listitem.setAttribute("class","counts");
 
+                for (var d in data){
+                    $(ulist).append('<li>'+data[d].value.trigger_on+'</li>');
+                }    
+
+                $(slidee.children(' .frame ')).append(ulist);
+                $(listitem).append(slidee);
+                $(utable).append(listitem);
+            }
+    }).done(function(){
+            loadTimeSlidee();    
+    });
+
+}
+
+function loadDetails(data,application){
+    
+    //initialize the dom elements
     var ulist = $('#app-items-'+application);
-    var table = '<li class="appwrap" id=wrap-'+application +'">'+
-        '<ul id="'+ data['value']['hash_key']+'" class="tablet"></ul>'+
-        '</li>';
+
+    var table  = document.createElement('li');
+    table.setAttribute("id","wrap-"+application);
+    table.setAttribute("class","appwrap");
+
+    var utable = document.createElement('ul');
+    utable.setAttribute("id",data.value.hash_key);
+    utable.setAttribute("class","tablet");
+
     ulist.append(table);
-    fixed_types = ["build_id", "slot", "handlers","paltform","test_env","test_group"];
-    var type = "";
+    $(table).append(utable);
+
+    fixed_types = ["build_id", "slot", "handlers", "platform","test_env","test_group"]; 
+    var type = ""; 
     var tableitem = "";
     var content = "";
-    var utable = $('#'+data['value']['hash_key']);
+
     
+    //only tale the fixed variables information
+
     for (var j in fixed_types){
         type = fixed_types[j];
-        content = data['value'][type];
+        content = data.value[type];
+        //if the type corresponds to an array
+        var listitem = document.createElement('li');
+        var bottom = document.createElement('div');
+        bottom.setAttribute("class","bottom");
+        var toppom = document.createElement('div');
+        toppom.setAttribute("class","top");
 
+        $(toppom).append('<h1>'+type+'</h1>');
+
+
+        
+        var fullline = '';
         if( Object.prototype.toString.call( content ) === '[object Array]' ) {
-            newslidee = type+"-"+data['value']['hash_key']+'-slidee';
-            var fullline = '';
+    
             for (var a in content){
                 fullline =  fullline+'<p>'+content[a]+'</p>';
-            }
-            tableitem = '<li>'+
-                        '<div class="top">'+
-                        '<h1>'+type+'</h1>'+
-                        '</div>'+
-                        '<div class="bottom">'+
-                        '<div id='+newslidee+'>'+fullline+'</p>'+
-                        '</div>'+
-                        '</li>';
 
+            }
+            var wrap = $('.newwrap').clone();
+            wrap.addClass("pagewrap");
+            $(wrap).removeClass("newwrap");
+            $((wrap.children(' .frame ')).children('.slidee')).append(fullline);
+
+            $(bottom).append(wrap);
+            $(bottom).append(wrap);
         }
         else{
-            tableitem = '<li>'+
-            '<div class="top">'+
-            '<h1>'+type+'</h1>'+
-            '</div>'+
-            '<div class="bottom">'+
-            '<p>'+content+'</p>'+
-            '</div>'+
-            '</li>';
+            fullline = content;
+            $(bottom).append('<div>'+fullline+'</div>');
         }
-        utable.append(tableitem);
 
-        console.log("appended the item");
+        $(listitem).append(toppom);
+        $(listitem).append(bottom);            
+ 
+        $(utable).append(listitem);
+
     }
+    //schedule time is undefined because the configuration file doesnt exist
+    loadTestResults(utable,data.value.filename,data.value.schedule_time);
+
 }
 
 function loadLatestNews(){
+
+    // request for the latest uploaded entries in the database
     $.get('/nightlies-periodic/latestNews/',function(data){
-            var data = jQuery.parseJSON(data);
+            data = jQuery.parseJSON(data);
 
             var ulist = $('.items');
             data=[ 'Brunel','Moore', 'DaVinci', 'Gauss'];
 
             for(var i in data){
-            ulist.append('<li>'+data[i]+'</li>');
+                ulist.append('<li>'+data[i]+'</li>');
             }
-            loadSlidee();
+                loadSlidee();
             });
 
 }
 
+//load the slidees inside the tabs container
+function loadTimeSlidee(){
+    var $frame = $(".timewrap").children(".frame");
+    var $wrap = $(".timewrap");
+
+    $frame.sly({
+        horizontal: 1,
+        itemNav: 'basic',
+        smart: 1,
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: 0,
+        scrollBar: $wrap.find('.scrollbar'),
+        scrollBy: 1,
+        speed: 300,
+        elasticBounds: 1,
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1
+    });
+}
+
+//load the slidees inside the tabs container
 function loadSubSlidee(tabid){
     var $frame = $("#"+tabid);
     var $slidee = $frame.children('ul').eq(0);
 
     $frame.sly({
-itemNav: 'basic',
-smart: 1,
-activateOn: 'click',
-mouseDragging: 1,
-touchDragging: 1,
-releaseSwing: 1,
-startAt: 0,
-scrollBy: 1,
-speed: 300,
-elasticBounds: 1,
-dragHandle: 1,
-dynamicHandle: 1,
-clickBar: 1
+        itemNav: 'basic',
+        smart: 1,
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: 0,
+        scrollBy: 1,
+        speed: 300,
+        elasticBounds: 1,
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1
 
 });
 }
 
+// load the slidee of the latest news
 function loadSlidee(){
 
     var $frame = $('#smart');
@@ -130,29 +213,46 @@ function loadSlidee(){
     var $wrap  = $frame.parent();
 
     $frame.sly({
-itemNav: 'basic',
-smart: 1,
-activateOn: 'click',
-mouseDragging: 1,
-touchDragging: 1,
-releaseSwing: 1,
-startAt: 3,
-scrollBar: $wrap.find('.scrollbar'),
-scrollBy: 1,
-speed: 300,
-elasticBounds: 1,
-dragHandle: 1,
-dynamicHandle: 1,
-clickBar: 1,
+        itemNav: 'basic',
+        smart: 1,
+        activateOn: 'click',
+        mouseDragging: 1,
+        touchDragging: 1,
+        releaseSwing: 1,
+        startAt: 3,
+        scrollBar: $wrap.find('.scrollbar'),
+        scrollBy: 1,
+        speed: 300,
+        elasticBounds: 1,
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1,
 
-//Buttons
-prev: $wrap.find('.prev'),
-next: $wrap.find('.next')
-});
+        //Buttons
+        prev: $wrap.find('.prev'),
+        next: $wrap.find('.next')
+    });
+}
+
+//ftiakse autonton kodika na kanei slidee tous handlers
+function loadPageSlidee(id){
+    var $frame = $(id);
+    var $wrap  = $frame.parent();
+    $frame.sly({
+        speed: 100,
+        easing: 'easeOutExpo',
+        activatePageOn: 'click',
+        scrollBar: $wrap.find('.scrollbar'),
+        scrollBy: 20,
+        dragHandle: 1,
+        dynamicHandle: 1,
+        clickBar: 1
+    });
 }
 
 $(document).ready(function($){
-        console.log("ready to start");
+        // load the tabs that coresponde to the main applications
         loadMainApplications();
+        // load the news in time order
         loadLatestNews();
-        });
+});
